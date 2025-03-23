5eff5df1a49dc12e19f2f1e30ffa31f9ca475dfe
luxeon <quitereality@gmail.com>
2025-03-23 10:26:49 +0100

    migrate to vite

--- fitnycrm-designer-ui/src/main/webapp/src/index.tsx
+++ fitnycrm-designer-ui/src/main/webapp/src/index.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
-reportWebVitals(); 
+); 
