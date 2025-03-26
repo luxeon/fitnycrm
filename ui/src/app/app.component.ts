44f30b45506624cc66832abad48be9bfe9fb6398
Mark Sierikov <rubrex@Marks-MacBook-Pro.local>
2025-03-26 01:32:19 +0100

    Add registration form

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegistrationComponent } from './features/auth/registration/registration.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  standalone: true,
+  imports: [RouterOutlet, RegistrationComponent],
+  template: '<app-registration></app-registration>',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
