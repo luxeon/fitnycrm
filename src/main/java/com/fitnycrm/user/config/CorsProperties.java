b418fdbc92f660aadd68b3238b6d5ca256e7add9
Mark Sierikov <rubrex@Marks-MacBook-Pro.local>
2025-03-27 19:52:04 +0100

    Add missed CorsProperties

---
+++ src/main/java/com/fitnycrm/user/config/CorsProperties.java
@@ -0,0 +1,18 @@
+package com.fitnycrm.user.config;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
+
+@Data
+@Validated
+@Configuration
+@ConfigurationProperties(prefix = "cors")
+public class CorsProperties {
+    @NotEmpty
+    private List<String> allowedOrigins;
+} 
