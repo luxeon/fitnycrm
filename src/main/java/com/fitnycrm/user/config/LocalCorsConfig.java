13799e0e803c0f58a359006aa4ee55bab2a51f63
Mark Sierikov <rubrex@Marks-MacBook-Pro.local>
2025-03-27 18:54:39 +0100

    Add injection of allowed CORS origins from config file

--- src/main/java/com/fitnycrm/user/config/LocalCorsConfig.java
+++ src/main/java/com/fitnycrm/user/config/LocalCorsConfig.java
@@ -1,5 +1,6 @@
 package com.fitnycrm.user.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,10 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Profile("local")
 @Configuration
+@RequiredArgsConstructor
 public class LocalCorsConfig implements WebMvcConfigurer {
+
+    private final CorsProperties corsProperties;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins(corsProperties.getAllowedOrigins().toArray(new String[0]))
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
