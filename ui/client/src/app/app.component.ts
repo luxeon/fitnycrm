8951873931633c58d3dd4b36649ec041350ca778
Mark Sierikov <rubrex@Marks-MacBook-Pro.local>
2025-04-16 22:29:06 +0200

    Add localisation configuration to client application

--- ui/client/src/app/app.component.ts
+++ ui/client/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'client';
+
+  constructor(private translate: TranslateService) {
+    // Set default language
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
 }
