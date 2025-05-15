e4116cadda0c03eb74761cdf26c13b746a1b43aa
Mark Sierikov <rubrex@Marks-MacBook-Pro.local>
2025-05-15 17:34:48 +0200

    Client application: fix encoding problem during JWT decoding

--- ui/client/src/app/core/services/auth.service.ts
+++ ui/client/src/app/core/services/auth.service.ts
@@ -56,7 +56,13 @@ export class AuthService {
     try {
       const base64Url = token.split('.')[1];
       const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-      return JSON.parse(window.atob(base64));
+      const jsonPayload = decodeURIComponent(
+        atob(base64)
+          .split('')
+          .map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+          .join('')
+      );
+      return JSON.parse(jsonPayload);
     } catch (e) {
       console.error('Error decoding token:', e);
       return null;
